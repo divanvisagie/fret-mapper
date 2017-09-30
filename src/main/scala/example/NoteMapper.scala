dd527eaf897c77e0e4289bdcab4c2e545f57292c
Divan Visagie <visagiedivan@gmail.com>
2017-09-30 16:20:09 +0200

    test all notes for notemapper next

--- src/main/scala/example/NoteMapper.scala
+++ src/main/scala/example/NoteMapper.scala
@@ -3,12 +3,25 @@ package example
 
 class NoteMapper {
 
-  def getNextNote(): Unit = {
+  val noteOrder = Array(
+    "C#", "D", "D#", "E",  "F",
+    "F#", "G",  "G#", "A", "A#", "B", "C"
+  )
 
+  def getNextNote(note: String): String = {
+    val noteIndex = noteOrder.indexOf(note.toUpperCase)
+
+    val nextIndex = if (noteIndex == noteOrder.length-1) {
+      0
+    } else {
+      noteIndex + 1
+    }
+
+    noteOrder(nextIndex)
   }
 }
 
 object NoteMapper {
-  def apply: NoteMapper = new NoteMapper()
+  def apply(): NoteMapper = new NoteMapper()
 }
 
--- src/test/scala/example/NoteMapperSpec.scala
+++ src/test/scala/example/NoteMapperSpec.scala
@@ -3,10 +3,26 @@ package example
 import org.scalatest.{FlatSpec, Matchers}
 
 class NoteMapperSpec extends FlatSpec with Matchers {
-  val noteMapper = NoteMapper
+  val noteMapper = NoteMapper()
+
+  def testNextNote(note: String, nextNote: String): Unit = {
+    s"getNextNote $note" should s"return $nextNote" in {
+      noteMapper.getNextNote(note) should be (nextNote)
+    }
+  }
+  testNextNote("A","A#")
+  testNextNote("A#","B")
+  testNextNote("B","C")
+  testNextNote("C","C#")
+  testNextNote("C#","D")
+  testNextNote("D#","E")
+  testNextNote("E", "F")
+  testNextNote("G", "G#")
+  testNextNote("G#","A")
+
 }
 
-class StringMapperSpect extends FlatSpec with Matchers {
+class StringMapperSpec extends FlatSpec with Matchers {
   val stringMapper = StringMapper()
 
   def testForNote(note: String): Unit = {
